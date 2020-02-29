8835c3c55faf426f0c81f81f551e81b4155d90a9
pierreelliott <pierreelliott.github@thiboud.fr>
2020-02-29 19:41:10 +0100

    Using Grid
    Instead of multiple 2D-arrays

---
+++ Grid.py
@@ -0,0 +1,84 @@
+from random import randint
+
+
+class Grid:
+    def __init__(self, rows, columns):
+        self.grid = [[Cell((i, j)) for i in range(columns)] for j in range(rows)]
+        self.ROW_SIZE = rows
+        self.COL_SIZE = columns
+
+    def get(self, pos: (int, int)):
+        """
+
+        :param pos: Position in the grid as (x, y)
+        :return:
+        """
+        x, y = pos
+        if x < 0 or y < 0 or x >= self.COL_SIZE or y >= self.ROW_SIZE:
+            raise Exception("Out of range")
+        return self.grid[y][x]
+
+    def set(self, pos: (int, int), obj: object):
+        """
+
+        :param obj:
+        :param pos: Position in the grid as (x, y)
+        :return:
+        """
+        x, y = pos
+        if x < 0 or y < 0 or x >= self.COL_SIZE or y >= self.ROW_SIZE:
+            raise Exception("Out of range")
+        self.grid[y][x] = obj
+
+    def get_random_empty_cell(self):
+        cell_found = False
+        x, y = 0, 0
+        # cell = Cell((x, y))
+        while not cell_found:
+            x = randint(0, self.COL_SIZE - 1)
+            y = randint(0, self.ROW_SIZE - 1)
+            cell = self.get((x, y))
+            cell_found = cell.empty()
+        return x, y
+
+    def __repr__(self):
+        # Print the grid
+        for i in range(self.ROW_SIZE):
+            for j in range(self.COL_SIZE):
+                print(self.grid[i][j], end=" ")
+            print()
+
+    def move(self, agent, pos: (int, int)):
+        if self.get(pos).available():
+            self.set_agent(agent.pos, None)
+            self.set_agent(pos, agent)
+            return True
+        else:
+            return False
+
+    def set_object(self, pos: (int, int), obj: object):
+        self.get(pos).obj = obj
+
+    def get_object(self, pos: (int, int)):
+        return self.get(pos).obj
+
+    def set_agent(self, pos: (int, int), agent):
+        self.get(pos).agent = agent
+        if agent is not None:
+            agent.pos = pos
+
+    def get_agent(self, pos: (int, int)):
+        return self.get(pos).agent
+
+
+class Cell:
+    def __init__(self, pos: (int, int)):
+        self.agent = None
+        self.obj = None
+        self.pos = pos
+
+    def empty(self):
+        return not self.obj and self.available()
+
+    def available(self):
+        return not self.agent
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from collections import deque, Counter
 from time import sleep
 from GridGraphics import GridPG
 from Agent import Agent
+from Grid import Grid
 
 random.seed(0)
 
@@ -16,10 +17,10 @@ MAX_MEMORY_SIZE = 10
 NEIGHBOURHOOD_SIZE = 2
 MAX_MOVES = 1
 MAX_ITER = int(1e7)
-K_PICK = 0.1    # k+
-K_PUT = 0.3     # k-
+K_PICK = 0.1  # k+
+K_PUT = 0.3  # k-
 
-REFRESH_FREQ = MAX_ITER//10
+REFRESH_FREQ = MAX_ITER // 10
 # REFRESH_FREQ = 10
 LOOK_AROUND = False
 
@@ -36,53 +37,52 @@ def random_position_in_grid(grid_size: int):
 
 def create_manhattan_distance(position: (int, int)):
     return lambda x, y: abs(x - position[0]) + abs(y - position[1])
+
+
 # ====================================================
 
 
 class Environment:
     def __init__(self, objects: [(object, int)] = (('A', 200), ('B', 200)), grid_size: int = 50):
         self.grid_size = grid_size
-        self.grid = [[None for i in range(grid_size)] for j in range(grid_size)]
-        self.agent_grid = [[None for i in range(grid_size)] for j in range(grid_size)]
+        self.grid = Grid(grid_size, grid_size)
+        # self.grid = [[None for i in range(grid_size)] for j in range(grid_size)]
+        # self.agent_grid = [[None for i in range(grid_size)] for j in range(grid_size)]
         if PYGAME:
             self.grid_graphics = GridPG(grid_size, grid_size, objects)
         for object_type, nb_object in objects:
             count = nb_object
             while count > 0:
-                x, y = random_position_in_grid(grid_size)
-                if self.grid[y][x] is None:
-                    self.grid[y][x] = object_type
-                    count -= 1
+                x, y = self.grid.get_random_empty_cell()
+                self.grid.set_object((x, y), object_type)
+                count -= 1
 
     def __repr__(self):
-        # Print the grid
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                print(self.grid[i][j], end=" ")
-            print()
+        self.grid.__repr__()
 
     def populate(self, agents: [Agent]):
         for agent in agents:
-            placed = False
-            while not placed:
-                x, y = random_position_in_grid(self.grid_size)
-                if self.agent_grid[y][x] is None:
-                    self.agent_grid[y][x] = agent
-                    agent.pos = (y, x)
-                    placed = True
+            x, y = self.grid.get_random_empty_cell()
+            self.grid.set_agent((x, y), agent)
+
+    def get_possible_moves(self, agent: Agent):
+        M = agent.moves
+        ranges = ((-M, 0), (M, 0), (0, -M), (0, M))
+        possible_moves = []
+        for r in ranges:
+            possible_moves.append(tuple(item1 + item2 for item1, item2 in zip(agent.pos, r)))
+        random.shuffle(possible_moves)
+        return possible_moves
 
     def move(self, agent: Agent):
-        possible_range = list(range(-agent.moves, agent.moves+1))     # (-2, -1, 0, 1, 2) for moves = 2
-        possible_range.remove(0)
+        moves = self.get_possible_moves(agent)
 
-        x = agent.pos[0] + random.choice(possible_range)
-        y = agent.pos[1] + random.choice(possible_range)
-        position = (x, y)
-
-        if 0 <= position[0] < self.grid_size and 0 <= position[1] < self.grid_size and self.get_a(position) is None:
-            self.set_a(agent.pos, None)
-            self.set_a(position, agent)
-            agent.pos = position
+        for move in moves:
+            try:
+                if self.grid.move(agent, move):
+                    return
+            except Exception:
+                pass
 
     def get_nb_objet_in_area(self, pos: tuple, obj_type: str):
         res = 0
@@ -105,32 +105,31 @@ class Environment:
         return res
 
     def get_obj(self, pos: tuple):
-        return self.grid[pos[0]][pos[1]]
+        return self.grid.get_object(pos)
 
     def set_obj(self, pos: tuple, obj):
-        self.grid[pos[0]][pos[1]] = obj
+        self.grid.set_object(pos, obj)
 
     def get_a(self, pos: tuple):
-        return self.agent_grid[pos[0]][pos[1]]
+        return self.grid.get_agent(pos)
 
     def set_a(self, pos: tuple, a):
-        self.agent_grid[pos[0]][pos[1]] = a
+        self.grid.set_agent(pos, a)
 
     def print_grids(self):
         s = []
-        for i, row in enumerate(self.grid):
+        for i, row in enumerate(self.grid.grid):
             for j, cell in enumerate(row):
-                agent = self.get_a((i, j))
                 if PYGAME:
-                    self.grid_graphics.draw_cell(j, i, cell, agent)
-                if agent is not None:
-                    if agent.carry:
+                    self.grid_graphics.draw_cell(j, i, cell.obj, cell.agent)
+                if cell.agent is not None:
+                    if cell.agent.carry:
                         s.append('§')
                     else:
                         s.append('+')
-                if cell is not None:
-                    s.append(cell)
-                elif agent is None:
+                if cell.obj is not None:
+                    s.append(cell.obj)
+                elif cell.agent is None:
                     s.append('.')
                 s.append('\t')
             s.append('\n')
@@ -147,26 +146,13 @@ def init_agents(nb_agents: int, environment: Environment) -> [Agent]:
     return agents
 
 
-def count_obj(env):
-    count = 0
-    for row in env.grid:
-        for cell in row:
-            if cell:
-                count += 1
-    for row in env.agent_grid:
-        for cell in row:
-            if cell and cell.carry:
-                count += 1
-    return count
-
-
 if __name__ == '__main__':
     env = Environment(objects=OBJECTS, grid_size=GRID_SIZE)
     agents = init_agents(NB_AGENTS, env)
     env.populate(agents)
     env.print_grids()
+    sleep(1)
     for it in range(MAX_ITER):
-    # for it in range(10000):
         agent = random.choice(agents)
         agent.act()
         env.move(agent)
